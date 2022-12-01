c1c5f1206e73fc6887109279d280b2707ccb7516
jiangchao <1392227049@qq.com>
2022-12-01 17:46:56 +0800

    build: demo

---
+++ dist/assets/detail.0a662b83.js
@@ -0,0 +1,1 @@
+import{aI as e,o as t,e as c}from"./index.d2ffc00c.js";const a={};function n(o,r){return t(),c("div",null,"detail")}const l=e(a,[["render",n]]);export{l as default};
---
+++ dist/assets/index.e53aefd4.js
@@ -0,0 +1,1 @@
+import{aI as e}from"./index.d2ffc00c.js";const r={};function c(n,t){return"123"}const o=e(r,[["render",c]]);export{o as default};
